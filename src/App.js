a8e56c0562f08b051ff7da575cde74e7dba617ce
Joshua Scott <joshua.ds.s019@gmail.com>
2024-01-17 13:14:37 -0800

    completed section of components, props, & state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState } from 'react'
+import Header from "./components/Header"
+import FeedbackList from "./components/FeedbackList"
+import FeedbackStats from "./components/FeedbackStats"
+
+import FeedbackData from './data/FeedbackData'
+
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const deleteFeedback = (id) => {
+        if (window.confirm('Are you sure you want to delete this post?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return (
-        <div className='container'>
-            <h1>My App</h1>
-        </div>
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback}
+                    handleDelete={deleteFeedback} />
+            </div>
+        </>
     )
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos dolor delectus praesentium atque velit quasi quis ut.'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos dolor delectus praesentium atque velit quasi quis ut.'
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dignissimos dolor delectus praesentium atque velit quasi quis ut.'
+    }
+]
+
+export default FeedbackData
